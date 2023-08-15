ade25842c2048948b5dff147b4c56bebd7e6cfc1
Jeetendra Patil <jspatil@amazon.com>
2023-08-15 15:37:10 -0700

    add cuda118

--- test/dlc_tests/ec2/pytorch/training/test_pytorch_training.py
+++ test/dlc_tests/ec2/pytorch/training/test_pytorch_training.py
@@ -209,6 +209,8 @@ def test_pytorch_train_dgl_cpu(pytorch_training, ec2_connection, cpu_only, py3_o
 @pytest.mark.parametrize("ec2_instance_type", PT_EC2_GPU_INSTANCE_TYPE, indirect=True)
 def test_pytorch_with_horovod(pytorch_training, ec2_connection, gpu_only, ec2_instance_type):
     _, image_framework_version = get_framework_and_version_from_tag(pytorch_training)
+    if Version(image_framework_version) in SpecifierSet(">=2.0"):
+        pytest.skip(f"Image {pytorch_training} doesn't package horovod. Hence test is skipped.")
     if "trcomp" in pytorch_training and Version(image_framework_version) in SpecifierSet("<2.0"):
         pytest.skip(f"Image {pytorch_training} doesn't package horovod. Hence test is skipped.")
     if test_utils.is_image_incompatible_with_instance_type(pytorch_training, ec2_instance_type):
@@ -231,6 +233,8 @@ def test_pytorch_with_horovod_inductor(
     _, image_framework_version = get_framework_and_version_from_tag(pytorch_training)
     if ec2_instance_type.startswith("g3"):
         pytest.skip("skipping inductor related test on g3 instance")
+    if Version(image_framework_version) in SpecifierSet(">=2.0"):
+        pytest.skip(f"Image {pytorch_training} doesn't package horovod. Hence test is skipped.")
     if "trcomp" in pytorch_training and Version(image_framework_version) in SpecifierSet("<2.0"):
         pytest.skip(f"Image {pytorch_training} doesn't package horovod. Hence test is skipped.")
     if test_utils.is_image_incompatible_with_instance_type(pytorch_training, ec2_instance_type):
